abfd2bdb1eb336da5731f99d5eafb345bdb2960e
brain-fuel <matt@brain-fuel.co>
2025-06-05 20:52:52 -0400

    fix not buffering stdin

--- src/cli/validate.ts
+++ src/cli/validate.ts
@@ -2,16 +2,25 @@ import { readFileSync } from "fs";
 import Ajv2020 from "ajv/dist/2020";
 import schema from "../../resources/ir.schema.json" assert { type: "json" };
 
-export function runValidate(file?: string) {
-  const useStdin = !file || file === "-";
+export async function runValidate(file?: string) {
+  let input = "";
 
-  if (useStdin && process.stdin.isTTY) {
-    console.error("No input provided");
-    process.exit(1);
+  if (!file || file === "-") {
+    if (process.stdin.isTTY) {
+      console.error("No input provided");
+      process.exit(1);
+    }
+
+    input = await new Promise<string>((resolve, reject) => {
+      let data = "";
+      process.stdin.setEncoding("utf-8");
+      process.stdin.on("data", chunk => (data += chunk));
+      process.stdin.on("end", () => resolve(data));
+      process.stdin.on("error", reject);
+    });
+  } else {
+    input = readFileSync(file, "utf-8");
   }
-
-  const input =
-    useStdin ? readFileSync(0, "utf-8") : readFileSync(file!, "utf-8");
   const json = JSON.parse(input);
   const ajv = new Ajv2020({ allErrors: true });
   const validate = ajv.compile(schema);
